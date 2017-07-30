18a9ff5daa344c1a700ca17ad56ffeee6534aaa3
eduardozel <edededed@yandex.ru>
2017-07-30 19:58:31 +0300

    Add files via upload

--- player.lua
+++ player.lua
@@ -15,8 +15,8 @@ function panelPlayer()
 -- ----
 	pnBUSY = wx.wxStaticBox(panelMP3, ID_PN_BUSY, "???", wx.wxPoint( 10, 10), wx.wxSize( 320, 50) )	
 
-    btnBUSY = wx.wxButton( pnBUSY, ID_BUTTON_BUSY, "busy",  wx.wxPoint( 15, 10), btnSize )
-    frame:Connect( ID_BUTTON_BUSY, wx.wxEVT_COMMAND_BUTTON_CLICKED, OnBUSY)	
+    btnBUSY = wx.wxButton( pnBUSY, ID_BUTTON_BUSY, "first track",  wx.wxPoint( 15, 10), btnSize )
+    frame:Connect( ID_BUTTON_BUSY, wx.wxEVT_COMMAND_BUTTON_CLICKED, On1Track)	
 	
     notebook:AddPage(panelMP3, "player")
 
@@ -24,13 +24,23 @@ end -- panelPHONE
 
 -- Handle the button event
 
-function OnBUSY(event)
-	print ("busy")
+function On1Track(event)
+	print ("first track ")
 	openCOM_HOST()
-	sendCOM_HOST( "AT+GSMBUSY=1\r") -- Forbid all incoming calls
-	local rpl = getRply()
-	print ("?"..rpl)
-	local rpl = getRply()
-	print ("?"..rpl)
+	sendCOM_HOST( string.char( 0x7E))
+	sendCOM_HOST( string.char( 0xFF))
+	sendCOM_HOST( string.char( 0x06))
+	sendCOM_HOST( string.char( 0x03))
+	sendCOM_HOST( string.char( 0x00))
+	sendCOM_HOST( string.char( 0x00))
+	sendCOM_HOST( string.char( 0x01))
+	sendCOM_HOST( string.char( 0xFF))
+	sendCOM_HOST( string.char( 0xE6))
+	sendCOM_HOST( string.char( 0xEF))
+
+--	local rpl = getRply()
+--	print ("?"..rpl)
+--	local rpl = getRply()
+--	print ("?"..rpl)
 	closeCOM_HOST()
 end -- OnBUSY(event)
